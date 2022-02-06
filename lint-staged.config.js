52aaacc72f876cec8a75c001f0dc500ab79fd2ed
Shane Moore <shane@shanemoore.me>
2022-02-06 21:06:58 +0000

    Add a publish script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  '*.ts': (filenames) => [
-    'tsc -p tsconfig.json --noEmit --emitDeclarationOnly false',
-    `eslint --cache --fix ${filenames.join(' ')}`,
-  ],
+  '*.ts': (filenames) => ['yarn typecheck', `eslint --cache --fix ${filenames.join(' ')}`],
   '*.js': 'eslint --cache --fix',
 };
